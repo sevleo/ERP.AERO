e4bc4df3fb090196c43a5acc6c9922ef826be7e5
Seva <seva.leonov@hotmail.com>
2024-07-01 22:23:07 +0700

    Initial project setup & server configuration

---
+++ src/app.ts
@@ -0,0 +1,14 @@
+import express from "express";
+import dotenv from "dotenv";
+
+dotenv.config();
+const app = express();
+const PORT = process.env.PORT;
+
+app.get("/", (req, res) => {
+  res.send("Hello, world!");
+});
+
+app.listen(PORT, () => {
+  console.log(`Server is running on port ${PORT}`);
+});
