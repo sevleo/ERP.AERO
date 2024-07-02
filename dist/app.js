f757bb34fcb43a6c2ec94ccb2adfa575ae3f316b
Seva <seva.leonov@hotmail.com>
2024-07-02 15:57:01 +0700

    Add navigation controller & index view

--- dist/app.js
+++ dist/app.js
@@ -7,8 +7,12 @@ const dotenv_1 = __importDefault(require("dotenv"));
 dotenv_1.default.config();
 const express_1 = __importDefault(require("express"));
 require("./db");
+const auth_1 = __importDefault(require("./routes/auth"));
+const body_parser_1 = __importDefault(require("body-parser"));
 const app = (0, express_1.default)();
 const PORT = process.env.PORT;
+app.use(body_parser_1.default.json());
+app.use("/api", auth_1.default);
 app.get("/", (req, res) => {
     res.send("Hello, world!");
 });
--- src/app.ts
+++ src/app.ts
@@ -1,12 +1,16 @@
 import dotenv from "dotenv";
 dotenv.config();
 import express from "express";
-
 import "./db";
+import authRoutes from "./routes/auth";
 import jwt, { JwtPayload } from "jsonwebtoken";
+import bodyParser from "body-parser";
 
 const app = express();
 const PORT = process.env.PORT;
+
+app.use(bodyParser.json());
+app.use("/api", authRoutes);
 
 app.get("/", (req, res) => {
   res.send("Hello, world!");
