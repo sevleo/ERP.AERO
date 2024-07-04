8f8c2da48960904bc925a40a16ae3b35fbfcd866
Seva <seva.leonov@hotmail.com>
2024-07-04 17:21:17 +0700

    Clean up

--- src/helpers/calculateLatency.ts
+++ src/helpers/calculateLatency.ts
@@ -1,6 +1,5 @@
 export function calculateLatency(): number {
   const start = process.hrtime();
-  // Simulate some processing time
   const end = process.hrtime(start);
   const latency = end[0] * 1e3 + end[1] * 1e-6; // Convert to milliseconds
   return latency;
