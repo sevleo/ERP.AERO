76d5d21b6a57641fce94488b5242cfca81894167
Seva <seva.leonov@hotmail.com>
2024-07-04 18:07:26 +0700

    Remove extra app listener

--- src/app.ts
+++ src/app.ts
@@ -49,6 +49,6 @@ app.use("/", mainRouter);
 const PORT = process.env.PORT || 3000;
 app.listen(PORT, () => console.log("app listening on port 3001!"));
 
-app.listen(PORT as any, "0.0.0.0", () => {
-  console.log(`Server is running on http://0.0.0.0:${PORT}`);
-});
+// app.listen(PORT as any, "0.0.0.0", () => {
+//   console.log(`Server is running on http://0.0.0.0:${PORT}`);
+// });
